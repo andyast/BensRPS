9fc0838001cda9133f0f00bd5a456cf72a8fedd4
Andy Steinmann <andyast@gmail.com>
2014-12-11 14:21:18 -0600

    Initial Commit

---
+++ BensRPS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BensRPS
+//
+//  Created by Andy Steinmann on 12/11/14.
+//  Copyright (c) 2014 DLS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ BensRPS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BensRPS
+//
+//  Created by Andy Steinmann on 12/11/14.
+//  Copyright (c) 2014 DLS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
