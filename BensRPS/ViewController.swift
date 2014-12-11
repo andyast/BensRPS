1af17e1db3ca9be59fb76f2c2b1f82df1137ddb5
Andy Steinmann <andyast@gmail.com>
2014-12-11 14:48:45 -0600

    Makin'  it work

--- BensRPS/ViewController.swift
+++ BensRPS/ViewController.swift
@@ -7,21 +7,41 @@
 //
 
 import Cocoa
-
 class ViewController: NSViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
+		self.imageView.image = NSImage(named: "rock")
+		
 	}
-
+	@IBOutlet var rock_button:NSButton!
+	@IBOutlet var imageView: NSImageView!
+	
 	override var representedObject: AnyObject? {
 		didSet {
 		// Update the view, if already loaded.
 		}
 	}
 
+	@IBAction func rock_click(sender:AnyObject?)
+	{
+		self.imageView.image = NSImage(named: "rock")
+	}
+
+	
+	@IBAction func paper_click(sender:AnyObject?)
+	{
+		self.imageView.image = NSImage(named: "paper")
+	}
+
+	
+	
+	@IBAction func scissor_click(sender:AnyObject?)
+	{
+		self.imageView.image = NSImage(named: "scissor")
+	}
 
 }
 
